d72b364046f65725cf7c074705a4325d414d3008
richa shah <shahricha723@gmail.com>
2023-04-07 10:04:51 +0530

    Initial components of compose

--- app/src/main/java/com/example/demojetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/demojetpackcompose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.demojetpackcompose
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -13,8 +14,13 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.example.demojetpackcompose.databinding.ActivityMainBinding
-import com.vv.jetpackcomposedemo.sampleui.SampleText
+import com.vv.jetpackcomposedemo.sampleui.*
 
 class MainActivity : ComponentActivity() {
 	@SuppressLint("MissingInflatedId")
@@ -22,7 +28,25 @@ class MainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 
 		setContent {
-			SampleText()
+			Column(
+				modifier = Modifier.wrapContentSize(),
+				verticalArrangement = Arrangement.Center,
+				horizontalAlignment = Alignment.CenterHorizontally
+			) {
+				SampleText()
+				SampleSpacer()
+				SampleTextField()
+				SampleSpacer()
+				SampleButton(context = LocalContext.current)
+				SampleSpacer()
+				SampleBox()
+				SampleSpacer()
+				SampleRadioGroup()
+				SampleSpacer()
+				SampleToggles()
+				SampleSpacer()
+				SampleSlider()
+			}
 		}
 
 	}
--- app/src/main/java/com/vv/jetpackcomposedemo/sampleui/ScreenUI.kt
+++ app/src/main/java/com/vv/jetpackcomposedemo/sampleui/ScreenUI.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -17,9 +18,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.*
@@ -37,24 +36,25 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.demojetpackcompose.R
-import com.google.accompanist.glide.rememberGlidePainter
 import com.example.demojetpackcompose.ui.theme.hintColor
 import com.example.demojetpackcompose.ui.theme.white
+import com.google.accompanist.glide.rememberGlidePainter
 
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ItemPreview() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        SampleButton(LocalContext.current)
-    }
+	Box(
+		modifier = Modifier.fillMaxSize(),
+		contentAlignment = Alignment.Center
+	) {
+		SampleButton(LocalContext.current)
+	}
 
 
 }
@@ -62,270 +62,358 @@ fun ItemPreview() {
 
 @Composable
 fun SampleText() {
-    Text(
-        text = "Sample Compose",
-        color = Color.Black,
-        fontSize = 26.sp,
-        fontFamily = FontFamily(Font(R.font.raleway_light)),
-        fontWeight = FontWeight.Bold,
-        textAlign = TextAlign.Center,
-        overflow = TextOverflow.Ellipsis,
-        maxLines = 1,
-        textDecoration = TextDecoration.Underline,
-        fontStyle = FontStyle.Italic,
-        lineHeight = 20.sp,
-        letterSpacing = 1.sp
-    )
+	Text(
+		text = "Sample Compose",
+		color = Color.Black,
+		fontSize = 26.sp,
+		fontFamily = FontFamily(Font(R.font.raleway_light)),
+		fontWeight = FontWeight.Bold,
+		textAlign = TextAlign.Center,
+		overflow = TextOverflow.Ellipsis,
+		maxLines = 1,
+		textDecoration = TextDecoration.Underline,
+		fontStyle = FontStyle.Italic,
+		lineHeight = 20.sp,
+		letterSpacing = 1.sp
+	)
 }
 
 
 @Composable
 fun SampleImage() {
-    Image(
-        painter = painterResource(id = R.drawable.compose),
-        contentDescription = "compose",
-        contentScale = ContentScale.Inside,
-        alignment = Alignment.Center,
-        alpha = 0.5f,
-        colorFilter = ColorFilter.tint(Color.Red)
-    )
+	Image(
+		painter = painterResource(id = R.drawable.compose),
+		contentDescription = "compose",
+		contentScale = ContentScale.Inside,
+		alignment = Alignment.Center,
+		alpha = 0.5f,
+		colorFilter = ColorFilter.tint(Color.Red)
+	)
 }
 
 
 @SuppressLint("CheckResult")
 @Composable
 fun SampleLoadImage() {
-    Image(
-        modifier = Modifier.size(200.dp),
-        painter = rememberGlidePainter(
-            request = "https://i.imgur.com/tGbaZCY.jpg",
-            requestBuilder = {
-                placeholder(R.drawable.ic_placeholder)
-                error(R.drawable.ic_error)
-            }
-        ),
-        contentDescription = "dog",
-        contentScale = ContentScale.FillBounds
-    )
+	Image(
+		modifier = Modifier.size(200.dp),
+		painter = rememberGlidePainter(
+			request = "https://i.imgur.com/tGbaZCY.jpg",
+			requestBuilder = {
+				placeholder(R.drawable.ic_placeholder)
+				error(R.drawable.ic_error)
+			}
+		),
+		contentDescription = "dog",
+		contentScale = ContentScale.FillBounds
+	)
+}
+
+@Composable
+fun SampleRadioGroup(){
+	var selected by remember { mutableStateOf("Male") }
+	Row {
+		RadioButton(selected = selected == "Male", onClick = { selected = "Male" })
+		Text(
+			textAlign = TextAlign.Justify,
+			text = "Male",
+			modifier = Modifier.clickable(onClick = { selected = "Male" }).padding(start = 4.dp)
+		)
+		Spacer(modifier = Modifier.size(4.dp))
+		RadioButton(selected = selected == "Female", onClick = { selected = "Female" })
+		Text(
+			textAlign = TextAlign.Center,
+			text = "Female",
+			modifier = Modifier.clickable(onClick = { selected = "Female" }).padding(start = 4.dp)
+		)
+	}
+}
+
+@Composable
+fun CircularProgressIndicator(
+	modifier: Modifier = Modifier,
+	color: Color = MaterialTheme.colors.primary,
+	strokeWidth: Dp = 2.dp
+) {
+}
+@Composable
+fun SampleSlider(){
+
+	var sliderState by remember { mutableStateOf(5f) }
+	Slider(value = sliderState, modifier = Modifier.fillMaxWidth().padding(8.dp),
+		valueRange = 0f..10f,
+		onValueChange = { newValue ->
+			sliderState = newValue
+		}
+	)
+
 }
 
 
+@Composable fun sliderWithSteps(){
+	var sliderState by remember { mutableStateOf(20f) }
+	Slider(value = sliderState, modifier = Modifier.fillMaxWidth().padding(8.dp),
+		valueRange = 0f..100f,
+		steps = 5,
+		onValueChange = { newValue ->
+			sliderState = newValue
+		}
+	)
+}
+@Composable fun SampleToggles(){
+	var checked by remember { mutableStateOf(true) }
+	var switched by remember { mutableStateOf(true) }
+	Row {
+		Checkbox(
+			checked = checked,
+			modifier = Modifier.padding(8.dp),
+			onCheckedChange = { checked = !checked })
+		Switch(
+			checked = switched,
+			modifier = Modifier.padding(8.dp),
+			onCheckedChange = { switched = it }
+		)
+	}
+}
 @Composable
 fun SampleButton(context: Context) {
-    Button(
-        enabled = true,
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = MaterialTheme.colors.primary,
-            contentColor = MaterialTheme.colors.white
-        ),
-        onClick = {
-            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
-        },
-    ) {
-        Text(
-            modifier = Modifier.padding(start = 8.dp),
-            text = stringResource(id = R.string.login),
-            fontSize = 18.sp
-        )
-    }
+	Column(
+		modifier = Modifier.wrapContentSize(),
+		verticalArrangement = Arrangement.Center,
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
+
+		Button(
+			enabled = true,
+			colors = ButtonDefaults.buttonColors(
+				backgroundColor = MaterialTheme.colors.primary,
+				contentColor = MaterialTheme.colors.white
+			),
+			onClick = {
+				Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+			},
+		) {
+			Icon(
+				contentDescription = "",
+				painter = painterResource(R.drawable.ic_login),
+				modifier = Modifier.padding(end = 1.dp)
+			)
+
+			Text(
+				modifier = Modifier.padding(start = 8.dp),
+				text = stringResource(id = R.string.jetpack_compose),
+				fontSize = 18.sp
+			)
+		}
+	}
 }
 
 
 @Composable
 fun SampleBox() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Image(
-            modifier = Modifier.fillMaxSize(),
-            painter = painterResource(id = R.drawable.bg),
-            contentDescription = "bg",
-            contentScale = ContentScale.FillBounds
-        )
-        Text(
-            text = "Jetpack Compose", fontSize = 32.sp, fontWeight = FontWeight.Bold
-        )
-    }
+	Box(
+		modifier = Modifier
+			.wrapContentSize(align = Alignment.TopStart)
+			.height(40.dp),
+		contentAlignment = Alignment.Center
+	) {
+		Image(
+			modifier = Modifier.fillMaxSize(),
+			painter = painterResource(id = R.drawable.bg),
+			contentDescription = "bg",
+			contentScale = ContentScale.FillBounds
+		)
+		Text(
+			text = "Jetpack Compose", fontSize = 22.sp, fontWeight =
+			FontWeight.SemiBold
+		)
+	}
 }
 
 @Composable
 fun SampleRow() {
-    Row(
-        modifier = Modifier.fillMaxSize(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.Top
-    ) {
-        Text(text = "Sample text 1")
-        Text(text = "Sample text 2")
-        Text(text = "Sample text 3")
+	Row(
+		modifier = Modifier.fillMaxSize(),
+		horizontalArrangement = Arrangement.SpaceBetween,
+		verticalAlignment = Alignment.Top
+	) {
+		Text(text = "Sample text 1")
+		Text(text = "Sample text 2")
+		Text(text = "Sample text 3")
 
-    }
+	}
 }
 
 @Composable
 fun SampleColumn() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.Start,
-    ) {
-        Text(text = "Sample text 1")
-        Text(text = "Sample text 2")
-        Text(text = "Sample text 3")
+	Column(
+		modifier = Modifier.fillMaxSize(),
+		verticalArrangement = Arrangement.Top,
+		horizontalAlignment = Alignment.Start,
+	) {
+		Text(text = "Sample text 1")
+		Text(text = "Sample text 2")
+		Text(text = "Sample text 3")
 
-    }
+	}
 }
-
 
 
 @Composable
 fun ItemView(
-    modifier: Modifier,
-    imgId: Int,
-    title: String,
-    subTitle: String
+	modifier: Modifier,
+	imgId: Int,
+	title: String,
+	subTitle: String
 ) {
-    Card(
-        modifier = modifier,
-        elevation = 8.dp,
-        shape = RoundedCornerShape(8.dp)
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                modifier = Modifier.weight(.3f),
-                painter = painterResource(id = imgId),
-                contentDescription = "demoImg"
-            )
-            Column(
-                modifier = Modifier.weight(.7f),
-                verticalArrangement = Arrangement.Center,
-            ) {
-                Text(
-                    text = title,
-                    fontWeight = FontWeight.Bold
-                )
-                Text(
-                    text = subTitle,
-                    fontWeight = FontWeight.Normal
-                )
-            }
-        }
-    }
+	Card(
+		modifier = modifier,
+		elevation = 8.dp,
+		shape = RoundedCornerShape(8.dp)
+	) {
+		Row(
+			horizontalArrangement = Arrangement.SpaceBetween,
+			verticalAlignment = Alignment.CenterVertically
+		) {
+			Image(
+				modifier = Modifier.weight(.3f),
+				painter = painterResource(id = imgId),
+				contentDescription = "demoImg"
+			)
+			Column(
+				modifier = Modifier.weight(.7f),
+				verticalArrangement = Arrangement.Center,
+			) {
+				Text(
+					text = title,
+					fontWeight = FontWeight.Bold
+				)
+				Text(
+					text = subTitle,
+					fontWeight = FontWeight.Normal
+				)
+			}
+		}
+	}
 }
 
 
 data class UserData(
-    val imgId: Int = R.drawable.compose,
-    val title: String = "Sample Title",
-    val subTitle: String = "This is dummy sub title text string"
+	val imgId: Int = R.drawable.compose,
+	val title: String = "Sample Title",
+	val subTitle: String = "This is dummy sub title text string"
 )
 
 fun dummyDataList(): List<UserData> {
-    val list: ArrayList<UserData> = ArrayList()
-    for (i in 1..30) {
-        list.add(UserData(title = "Sample Title $i"))
-    }
-    return list
+	val list: ArrayList<UserData> = ArrayList()
+	for (i in 1..30) {
+		list.add(UserData(title = "Sample Title $i"))
+	}
+	return list
 }
 
 @Composable
 fun SampleLazyColumn() {
-    LazyColumn() {
-        items(dummyDataList()) {
-            ItemView(
-                modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp),
-                imgId = it.imgId,
-                title = it.title,
-                subTitle = it.subTitle
-            )
-        }
-    }
+	LazyColumn() {
+		items(dummyDataList()) {
+			ItemView(
+				modifier = Modifier.padding(
+					horizontal = 12.dp,
+					vertical = 6.dp
+				),
+				imgId = it.imgId,
+				title = it.title,
+				subTitle = it.subTitle
+			)
+		}
+	}
 }
 
 @Composable
 fun SampleLazyRow() {
-    LazyRow() {
-        items(dummyDataList()) {
-            ItemView(
-                modifier = Modifier
-                    .width(300.dp)
-                    .padding(horizontal = 12.dp, vertical = 6.dp),
-                imgId = it.imgId,
-                title = it.title,
-                subTitle = it.subTitle
-            )
-        }
-    }
+	LazyRow() {
+		items(dummyDataList()) {
+			ItemView(
+				modifier = Modifier
+					.width(300.dp)
+					.padding(horizontal = 12.dp, vertical = 6.dp),
+				imgId = it.imgId,
+				title = it.title,
+				subTitle = it.subTitle
+			)
+		}
+	}
+}
+@Composable
+fun SampleSpacer(modifier:Modifier=Modifier.size(2.dp)) {
+	Spacer(modifier = Modifier.size(2.dp))
 }
 
 
 @Composable
 fun SampleTextField() {
-    val name = remember { (mutableStateOf("")) }
-    TextField(
-        value = name.value,
-        onValueChange = {
-            name.value = it
-        },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(5.dp),
-        textStyle = TextStyle(
-            color = Color.Black,
-            fontSize = 18.sp,
-            fontFamily = FontFamily(Font(R.font.raleway_bold))
-        ),
-        readOnly = false,
-        enabled = true,
-        label = {
-            Text(
-                text = stringResource(id = R.string.user_name),
-                color = MaterialTheme.colors.hintColor
-            )
-        },
-        placeholder = {
-            Text(
-                text = stringResource(id = R.string.enter_username),
-                color = MaterialTheme.colors.hintColor
-            )
-        },
-        leadingIcon = {
-            Icon(Icons.Default.Person, contentDescription = "icon")
-        },
-        trailingIcon = {
-            Icon(Icons.Default.Info, contentDescription = "icon")
-        },
-        singleLine = true,
-        isError = false,
-        visualTransformation = VisualTransformation.None,//PasswordVisualTransformation(),
-        keyboardOptions = KeyboardOptions(
-            capitalization = KeyboardCapitalization.None,
-            keyboardType = KeyboardType.Email,
-            imeAction = ImeAction.Done
-        ),
-        colors = TextFieldDefaults.textFieldColors(
-            focusedIndicatorColor = Color.Black
-        )
-    )
+	val name = remember { (mutableStateOf("")) }
+	TextField(
+		value = name.value,
+		onValueChange = {
+			name.value = it
+		},
+		modifier = Modifier
+			.fillMaxWidth()
+			.padding(5.dp),
+		textStyle = TextStyle(
+			color = Color.Black,
+			fontSize = 18.sp,
+			fontFamily = FontFamily(Font(R.font.raleway_bold))
+		),
+		readOnly = false,
+		enabled = true,
+		label = {
+			Text(
+				text = stringResource(id = R.string.user_name),
+				color = MaterialTheme.colors.hintColor
+			)
+		},
+		placeholder = {
+			Text(
+				text = stringResource(id = R.string.enter_username),
+				color = MaterialTheme.colors.hintColor
+			)
+		},
+		leadingIcon = {
+			Icon(Icons.Default.Person, contentDescription = "icon")
+		},
+		trailingIcon = {
+			Icon(Icons.Default.Info, contentDescription = "icon")
+		},
+		singleLine = true,
+		isError = false,
+		visualTransformation = VisualTransformation.None,//PasswordVisualTransformation(),
+		keyboardOptions = KeyboardOptions(
+			capitalization = KeyboardCapitalization.None,
+			keyboardType = KeyboardType.Email,
+			imeAction = ImeAction.Done
+		),
+		colors = TextFieldDefaults.textFieldColors(
+			focusedIndicatorColor = Color.Black
+		)
+	)
 }
 
 
 @SuppressLint("MissingInflatedId")
 @Composable
 fun DisplayView() {
-    AndroidView(modifier = Modifier.fillMaxSize(),
-        factory = {
-            val view = LayoutInflater.from(it).inflate(R.layout.activity_main, null, false)
-            val txtSample = view.findViewById<TextView>(R.id.txtSample)
-            val imgSample = view.findViewById<ImageView>(R.id.imgSample)
-            txtSample.text = it.getText(R.string.app_name)
-            imgSample.setImageDrawable(it.getDrawable(R.drawable.compose))
-            view
-        })
+	AndroidView(modifier = Modifier.fillMaxSize(),
+		factory = {
+			val view = LayoutInflater.from(it)
+				.inflate(R.layout.activity_main, null, false)
+			val txtSample = view.findViewById<TextView>(R.id.txtSample)
+			val imgSample = view.findViewById<ImageView>(R.id.imgSample)
+			txtSample.text = it.getText(R.string.app_name)
+			imgSample.setImageDrawable(it.getDrawable(R.drawable.compose))
+			view
+		})
 
 }
 
